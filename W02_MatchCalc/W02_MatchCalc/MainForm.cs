2d874f1479c2e37ed6d0430bfcdf0d88e3e05c2c
Joseph Gale <gal16026@byui.edu>
2020-01-17 13:31:52 -0600

    first math quiz upload

---
+++ W02_MatchCalc/W02_MatchCalc/MainForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace W02_MatchCalc
+{
+    public partial class MainForm : Form
+    {
+     
+
+        //Create a Random object called randomizer
+        //to generate random numbers
+        Random randomizer = new Random();
+
+        //Create integers for addition problem
+        int addend1;
+        int addend2;
+
+        // These integer variables store the numbers 
+        // for the subtraction problem. 
+        int minuend;
+        int subtrahend;
+
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+
+        //Create integer for remaining time
+        int timeLeft;
+
+        //start the quiz
+        public void StartTheQuiz()
+        {
+            //reset color: 
+            timeLabel.BackColor = Color.White;
+            //fill in addition problem with 2 random numbers
+            //store in addend1 and addend2
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            //convert to string for display
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            //make sure NumericUpDown is set to 0 for sum
+            sum.Value = 0;
+
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            //start the timer
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+        }
+
+        /// <summary>
+        /// Check the answer to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+  
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                //on 5 seconds, start red color background:
+                if (timeLeft <= 6)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+                // If CheckTheAnswer() return false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft--;
+                timeLabel.Text = timeLeft + " seconds";
+            }            
+            else
+            {
+                // If the user ran out of time, stop the timer, show 
+                // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e) //sender=NumericUpDown control
+        {
+            // Select the whole answer in the NumericUpDown control.
+            //couldn't we have just changed parameter to (NumericUpdown sender, EventArgs e)?
+            //would polymorphism allow NumericUpDown answerBox = sender;
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+
+        }
+
+        private void difference_ValueChanged(object sender, EventArgs e)
+        {
+            if (minuend - subtrahend == difference.Value)
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void sum_ValueChanged(object sender, EventArgs e)
+        {
+
+            if (addend1 + addend2 == sum.Value)
+            {
+                SystemSounds.Beep.Play();
+            }
+
+        }
+
+        private void product_ValueChanged_1(object sender, EventArgs e)
+        {
+            if (multiplicand * multiplier == product.Value)
+            {
+                SystemSounds.Beep.Play();
+            }
+
+        }
+
+        private void quotient_ValueChanged(object sender, EventArgs e)
+        {
+            if (dividend / divisor == quotient.Value)
+            {
+                SystemSounds.Beep.Play();
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+            label4.Text = DateTime.Now.ToString("dd MMMM yyyy");
+        }
+
+    }
+}

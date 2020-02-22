d381271047d43e2b9a12a7b375e822fd98340a04
Joseph Gale <gal16026@byui.edu>
2020-02-22 12:21:51 -0600

    functioning

---
+++ MyScriptureJournal/Models/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyScriptureJournal.Data;
+using System;
+using System.Linq;
+
+namespace MyScriptureJournal.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MyScriptureJournalContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MyScriptureJournalContext>>()))
+            {
+                // Look for any scriptures
+                if (context.Scripture.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Scripture.AddRange(
+                    new Scripture
+                    {
+                        Book = "1 Nephi",
+                        Chapter = 3,
+                        Verse = 7,
+                        Notes = "#obedience #obey",
+                        AddedDate = DateTime.Parse("2020-2-12"),
+                    },
+                    new Scripture
+                    {
+                        Book = "2 Nephi",
+                        Chapter = 2,
+                        Verse = 25,
+                        Notes = "#joy",
+                        AddedDate = DateTime.Parse("2020-2-12"),
+                    },
+                    new Scripture
+                    {
+                        Book = "Mosiah",
+                        Chapter = 2,
+                        Verse = 32,
+                        Notes = "#obedience #obey",
+                        AddedDate = DateTime.Parse("2020-2-12"),
+                    },
+                    new Scripture
+                    {
+                        Book = "Genesis",
+                        Chapter = 2,
+                        Verse = 16,
+                        Notes = "#agency #choice",
+                        AddedDate = DateTime.Parse("2020-2-12"),
+                    },
+                    new Scripture
+                    {
+                        Book = "2 Nephi",
+                        Chapter = 2,
+                        Verse = 27,
+                        Notes = "#agency #free",
+                        AddedDate = DateTime.Parse("2020-2-12"),
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+    
+
